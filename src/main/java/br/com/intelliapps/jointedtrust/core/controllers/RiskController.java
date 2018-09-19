ea0933b46f48f384435ea7bc5b8143a05d3f8d21
Rafael Estevam <rafael.estevam@exedconsulting.com>
2018-09-19 14:23:55 -0300

    Funcionalidade de upload via arquivo CSV - Riscos

--- src/main/java/br/com/intelliapps/jointedtrust/core/controllers/RiskController.java
+++ src/main/java/br/com/intelliapps/jointedtrust/core/controllers/RiskController.java
@@ -1,7 +1,9 @@
 package br.com.intelliapps.jointedtrust.core.controllers;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -45,36 +47,36 @@ import br.com.intelliapps.jointedtrust.main.utils.MediaTypeUtils;
 @Controller
 @RequestMapping("/risk")
 public class RiskController {
-	
+
 	@Autowired
 	private RiskService riskService;
-	
+
 	@Autowired
 	private MessageSource messageSource;
-	
+
 	@Autowired
 	private Locale locale;
-	
+
 	@Autowired
 	private FileSaverComponent fileSaver;
-	
+
 	@Autowired
 	private LoggedUserService loggedUserService;
-	
+
 	@Autowired
 	private ServletContext servletContext;
-	
+
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
 		binder.addValidators(new RiskValidator());
 	}
-	
-	@RequestMapping(value="/show", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/show", method = RequestMethod.GET)
 	public String showRisk(@RequestParam("guid") String guid, Model model) {
-		
+
 		Risk risk = riskService.findByGuid(guid);
 		model.addAttribute("risk", risk);
-		
+
 		return "risk";
 	}
 
@@ -84,117 +86,154 @@ public class RiskController {
 		model.addAttribute("risks", risks);
 		return "risks";
 	}
-	
-	@RequestMapping(value="/create", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
 	public String riskForm(Risk risk, Model model) {
 		return "newrisk";
 	}
-	
-	@RequestMapping(value="/rest/create", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/rest/create", method = RequestMethod.POST)
 	public ResponseEntity<?> createRiskByRest(@Valid Risk risk, BindingResult binding, RedirectAttributes rAttr, @RequestParam MultipartFile[] file, Model model) {
 		System.out.println(file.length);
-		
+
 		MultiValueMap<String, Object> responseMap = new LinkedMultiValueMap<>();
-		
-		if(binding.hasErrors()) {
-			binding.getFieldErrors()
-				.stream()
-				.forEach(
-					e -> {
-						String errorMessage = messageSource.getMessage(e.getCode(), new String[] {}, locale);
-						responseMap.add(e.getField(), errorMessage);
-					}
-				);
+
+		if (binding.hasErrors()) {
+			binding.getFieldErrors().stream().forEach(e -> {
+				String errorMessage = messageSource.getMessage(e.getCode(), new String[] {}, locale);
+				responseMap.add(e.getField(), errorMessage);
+			});
 			return new ResponseEntity<>(responseMap, HttpStatus.INTERNAL_SERVER_ERROR);
-			//return this.riskForm(risk);
+			// return this.riskForm(risk);
 		}
-		
-		if(riskService.existsByRiskId(risk.getRisk_id())) {
+
+		if (riskService.existsByRiskId(risk.getRisk_id())) {
 			String errorMessage = messageSource.getMessage("valid.risk.error.riskid.exists", new String[] {}, locale);
 			responseMap.add("risk_id", errorMessage);
 			return new ResponseEntity<>(responseMap, HttpStatus.INTERNAL_SERVER_ERROR);
-			//return this.riskForm(risk);
+			// return this.riskForm(risk);
 		}
-		
+
 		risk.setGuid(this.getGuid());
-		
-		//Adding Files related to Risk
+
+		// Adding Files related to Risk
 		List<DocFile> fileList = new ArrayList<DocFile>();
 		Map<String, String> filesMap = fileSaver.write("risk-docs", file, risk.getName());
-		filesMap.entrySet()
-			.forEach( entry -> {
-				DocFile newFile = new DocFile(entry.getKey(), entry.getValue());
-				int offset = entry.getKey().length() - 3;
-				int total = entry.getKey().length();
-				String fileType = entry.getKey().substring(offset, total);
-				newFile.setFileType(fileType.toUpperCase());
-				newFile.setUploadedBy(loggedUserService.loggedUser().getName().toUpperCase());
-				fileList.add(newFile);
-			});
+		filesMap.entrySet().forEach(entry -> {
+			DocFile newFile = new DocFile(entry.getKey(), entry.getValue());
+			int offset = entry.getKey().length() - 3;
+			int total = entry.getKey().length();
+			String fileType = entry.getKey().substring(offset, total);
+			newFile.setFileType(fileType.toUpperCase());
+			newFile.setUploadedBy(loggedUserService.loggedUser().getName().toUpperCase());
+			fileList.add(newFile);
+		});
 		risk.setFiles(fileList);
-		
+
 		riskService.save(risk);
 		String successMessage = messageSource.getMessage("notification.risk.create.success", new String[] { risk.getName() }, locale);
 		String successMessageTitle = messageSource.getMessage("notification.risk.create.success.title", new String[] {}, locale);
-		//rAttr.addFlashAttribute("successMessage", successMessage);
-		
+		// rAttr.addFlashAttribute("successMessage", successMessage);
+
 		responseMap.add("successTitle", successMessageTitle);
 		responseMap.add("successMessage", (String) successMessage);
 		return new ResponseEntity<>(responseMap, HttpStatus.OK);
-		//return "redirect:/risk";
-		//return this.riskList(model);
+		// return "redirect:/risk";
+		// return this.riskList(model);
 	}
-	
-	@RequestMapping(value="/create", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	public String createRisk(@Valid Risk risk, BindingResult binding, RedirectAttributes rAttr, @RequestParam MultipartFile[] file, Model model) {
 		System.out.println(file.length);
-		
-		if(binding.hasErrors()) {
+
+		if (binding.hasErrors()) {
 			return this.riskForm(risk, model);
 		}
-		
-		if(riskService.existsByRiskId(risk.getRisk_id())) {
+
+		if (riskService.existsByRiskId(risk.getRisk_id())) {
 			binding.rejectValue("risk_id", "valid.risk.error.riskid.exists");
 			return this.riskForm(risk, model);
 		}
-		
+
 		risk.setGuid(this.getGuid());
-		//riskService.save(risk);
+		// riskService.save(risk);
 		String successMessage = messageSource.getMessage("notification.risk.create.success", new String[] { risk.getName() }, locale);
 		String successMessageTitle = messageSource.getMessage("notification.risk.create.success.title", new String[] {}, locale);
 		rAttr.addFlashAttribute("successMessage", successMessage);
 		rAttr.addFlashAttribute("successMessageTitle", successMessageTitle);
-		
+
 		return "redirect:/risk/list";
 	}
-	
+
 	private String getGuid() {
 		return UUID.randomUUID().toString();
 	}
-	
-	@RequestMapping(value="/download", method=RequestMethod.GET)
-	public ResponseEntity<InputStreamResource> downloadFile(@RequestParam("filePath") String filePath){
-		
+
+	@RequestMapping(value = "/download", method = RequestMethod.GET)
+	public ResponseEntity<InputStreamResource> downloadFile(@RequestParam("filePath") String filePath) {
+
 		try {
 			Path path = Paths.get(filePath);
 			MediaType mediaType = MediaTypeUtils.getMediaTypeForFileName(servletContext, path.getFileName().toString());
 			File file = new File(filePath);
 			InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
-			
-			return ResponseEntity.ok()
-					.header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + path.getFileName().toString())
-					.contentType(mediaType)
-					.body(resource);
-			
-		}catch(Exception e) {
+
+			return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + path.getFileName().toString()).contentType(mediaType).body(resource);
+
+		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-		
+
 	}
-	
-	@RequestMapping(value="/batch", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/batch", method = RequestMethod.GET)
 	public String batchLoad() {
 		return "riskbatch";
 	}
-	
+
+	@RequestMapping(value = "/rest/batch", method = RequestMethod.POST)
+	public ResponseEntity<?> batchByRest(@RequestParam("file") MultipartFile file) {
+		
+		MultiValueMap<String, Object> responseMap = new LinkedMultiValueMap<>();
+		try {
+			List<Risk> riskList = extractRisks(file);
+			riskList.forEach(risk -> {
+					risk.setGuid(getGuid());
+					riskService.save(risk);
+			});
+			
+			String successMessage = messageSource.getMessage("notification.risk.batch.create.success", new String[] {}, locale);
+			String successMessageTitle = messageSource.getMessage("notification.risk.create.success.title", new String[] {}, locale);
+			responseMap.add("successTitle", successMessageTitle);
+			responseMap.add("successMessage", (String) successMessage);
+			return new ResponseEntity<>(responseMap, HttpStatus.OK);
+		}catch(Exception ex) {
+			String errorMessage = messageSource.getMessage("notification.risk.batch.create.error", new String[] {}, locale);
+			return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+
+	private List<Risk> extractRisks(MultipartFile file) throws Exception {
+		List<Risk> riskList = new ArrayList<Risk>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()));
+		String line;
+		int count = 0;
+		while ((line = br.readLine()) != null) {
+			String[] riskData = line.split(";");
+			if(count == 0) {
+				count += 1;
+				continue; //Escaping header line
+			}
+			Risk risk = new Risk();
+			risk.setRisk_id(riskData[0]);
+			risk.setName(riskData[1]);
+			risk.setDescription(riskData[2]);
+			risk.setCause(riskData[3]);
+			risk.setConsequence(riskData[4]);
+			riskList.add(risk);
+		}
+		return riskList;
+	}
+
 }
--- src/main/resources/static/assets/js/riskBatch.js
+++ src/main/resources/static/assets/js/riskBatch.js
@@ -1,3 +1,36 @@
 /**
  * Customizations for Risk Batch Page
  */
+
+//Callback events for Dropzone
+var myDropzone = Dropzone.forElement("#dzFiles");
+myDropzone.on("sending", function(file, obj, formData){
+	debugger;
+	for(var j = 0; j < file.length; j++){
+		formData.append("file", file[j]);
+	}
+});
+myDropzone.on("success", function(data){
+	console.log("SUCCESS...");
+	var responseMsg = JSON.parse(data.xhr.responseText)
+	$("#responseMessage").text(responseMsg.successMessage[0]);
+	$("#responseMessage").addClass("alert-success");
+	$("#responseMessageTitle").text(responseMsg.successTitle[0]);
+	$("#modalMessage").modal("show");
+});
+
+//Redirecting page on Modal Hidden
+$("#modalMessage").on("hidden.bs.modal", function(e){
+	$(location).attr('href', returnAddr);
+});
+
+//Submitting Form Event
+$("#batchForm").on("submit", function(e){
+	debugger;
+	var myDropzone = Dropzone.forElement("#dzFiles");
+	if(myDropzone.files.length > 0){
+		e.stopPropagation();
+		e.preventDefault();
+		myDropzone.processQueue();
+	}
+});
